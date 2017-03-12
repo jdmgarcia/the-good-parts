618b95e189fb43b62cfb5dff00a2442753fd756d
Juan Garcia <jdmgarciamoreno@gmail.com>
2017-03-12 18:26:26 +0000

    Start the project

---
+++ js/test.js
@@ -0,0 +1,37 @@
+// JavaScript
+// 
+
+var statement = {
+  type: '',
+  get: function(){
+    console.log('Value of type: ' + this.type);
+  },
+  set: function(newType){
+    this.type = newType;
+  },
+  appendChild: function(num, type = 'div') {
+    var newLayer = document.createElement(type), secuence = num;
+
+    for (var i =  num - 1; i > 0; i--) {
+      secuence += ' - ' + i;
+    }
+    newLayer.innerHTML = secuence;
+    document.getElementById(this.type).appendChild(newLayer);
+  }
+}
+
+function load(typeStatement) {
+  var value = '';
+
+  switch(typeStatement) {
+    case 'switch': 
+      value = document.getElementById('inputSwitch').value;
+      statement.set(typeStatement);
+      statement.appendChild(value);
+      statement.get();
+      break;
+
+    default:
+      console.log('No option available in switch: ' + typeStatement);
+  }
+}
