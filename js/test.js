e95312e85ea11ff6465f20d3367267dc280089b6
Juan Garcia <jdmgarciamoreno@gmail.com>
2017-03-13 14:01:40 +0000

    Added expressions and exceptions

--- js/test.js
+++ js/test.js
@@ -1,37 +1,117 @@
 // JavaScript
-// 
+//
 
-var statement = {
-  type: '',
-  get: function(){
-    console.log('Value of type: ' + this.type);
-  },
-  set: function(newType){
-    this.type = newType;
-  },
-  appendChild: function(num, type = 'div') {
-    var newLayer = document.createElement(type), secuence = num;
+// Object with functions as methods
+//
+let node = {
+    action: '',
+    get: function () {
+        console.log('Value of action: ' + this.action);
+    },
+    set: function (newAction) {
+        this.action = newAction;
+    },
+    appendChild: function (text, type = 'span') {
+        let newLayer = document.createElement(type);
 
-    for (var i =  num - 1; i > 0; i--) {
-      secuence += ' - ' + i;
+        newLayer.innerHTML = text;
+        document.getElementById(this.action).appendChild(newLayer);
+    },
+    printObj: function () {
+        let myObj = '', myProp;
+
+        for (myProp in this) {
+            if (this.hasOwnProperty(myProp)) {
+                myObj += myProp + '<br />';
+            }
+        }
+
+        return myObj;
     }
-    newLayer.innerHTML = secuence;
-    document.getElementById(this.type).appendChild(newLayer);
-  }
-}
+};
 
-function load(typeStatement) {
-  var value = '';
+// Return a decreasing secuence starting in `num`
+//
+let secuence = function (num) {
+    let secuence = num, i;
 
-  switch(typeStatement) {
-    case 'switch': 
-      value = document.getElementById('inputSwitch').value;
-      statement.set(typeStatement);
-      statement.appendChild(value);
-      statement.get();
-      break;
+    for (i = num - 1; i > 0; i--) {
+        secuence += ' - ' + i;
+    }
 
-    default:
-      console.log('No available option in switch: ' + typeStatement);
-  }
-}
+    return secuence;
+};
+
+// Remove content of HTML tag with id = `id`
+//
+let removeHTML = function (id) {
+    document.getElementById(id).innerHTML = '';
+};
+
+// Return if the parameter is a number
+//
+let isNumeric = function (input) {
+    let re = /^-{0,1}\d*\.{0,1}\d+$/;
+
+    return (re.test(input));
+};
+
+// Add two numbers
+// If one of the parameters at least is not a number, it return a exception message
+let add = function (num1, num2) {
+    if (!isNumeric(num1) || !isNumeric(num2)) {
+        throw {
+            name: 'TypeError',
+            message: 'add needs numbers'
+        };
+    }
+    return Number(num1) + Number(num2);
+};
+
+// Add two numbers with security
+//
+let controlAdd = function (num1, num2) {
+    let sum;
+
+    try {
+        sum = add(num1, num2);
+    } catch (e) {
+        sum = '<strong style="color: firebrick">' + e.name + ': ' + e.message + '</strong>';
+    }
+
+    return sum;
+};
+
+// Control of the action buttons
+//
+let load = function (typeStatement) {
+    let value = '', num1, num2;
+
+    node.set(typeStatement);
+    node.get();
+
+    switch (typeStatement) {
+        case 'switch':
+            value = document.getElementById('inputSwitch').value;
+
+            removeHTML(typeStatement);
+            node.appendChild(secuence(value));
+            break;
+
+        case 'for':
+            removeHTML(typeStatement);
+            node.appendChild(node.printObj(), 'strong');
+            break;
+
+        case 'exception':
+            num1 = document.getElementById('inputException1').value;
+            num2 = document.getElementById('inputException2').value;
+
+            removeHTML(typeStatement);
+            node.appendChild(controlAdd(num1, num2));
+            break;
+
+        default:
+            console.log('No available option in switch: ' + typeStatement);
+    }
+};
